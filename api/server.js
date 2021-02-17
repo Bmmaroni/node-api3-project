558170f1d1c9d0332bd36c2b16694ac5fdeb0935
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-02-16 18:12:46 -0700

    finished all middleware

--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const postsRouter = require('./posts/posts-router')
+const usersRouter = require('./users/users-router')
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
 
+server.use(express.json())
+server.use(postsRouter)
+server.use(usersRouter)
+
 // global middlewares and routes need to be connected here
+
+server.use((err, req, res, next) => {
+  console.log(err)
+
+  res.status(500).json({
+    message: "OOPS something went wrong on our end"
+  })
+})
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
