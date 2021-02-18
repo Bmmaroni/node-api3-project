777fdcf441a150fac057299589551682b20d2c6b
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-02-18 13:22:46 -0700

    Update console log for server listen

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./api/server')
 const port = process.env.PORT || 5000
 
 server.listen(port, () => {
-  console.log("running at http://localhost:5000")
+  console.log(`running at http://localhost:${port}`)
 })
